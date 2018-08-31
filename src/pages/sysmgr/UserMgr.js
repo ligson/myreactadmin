2366f8a572bcfc27b0ba63c5d3047c5eba3c4fc7
ligson <ligson@aliyun.com>
2018-08-31 16:43:38 +0800

    完善一下

--- src/pages/sysmgr/UserMgr.js
+++ src/pages/sysmgr/UserMgr.js
@@ -1,5 +1,60 @@
 import React from 'react';
 import MyView from "../../components/MyView";
+import {Divider, Table, Tag, Button} from "antd"
+
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  key: 'name',
+  render: text => <a href="javascript:;">{text}</a>,
+}, {
+  title: 'Age',
+  dataIndex: 'age',
+  key: 'age',
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+  key: 'address',
+}, {
+  title: 'Tags',
+  key: 'tags',
+  dataIndex: 'tags',
+  render: tags => (
+    <span>
+      {tags.map(tag => <Tag color="blue" key={tag}>{tag}</Tag>)}
+    </span>
+  ),
+}, {
+  title: 'Action',
+  key: 'action',
+  render: (text, record) => (
+    <span>
+      <a href="javascript:;">Invite {record.name}</a>
+      <Divider type="vertical"/>
+      <a href="javascript:;">Delete</a>
+    </span>
+  ),
+}];
+
+const data = [{
+  key: '1',
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+  tags: ['nice', 'developer'],
+}, {
+  key: '2',
+  name: 'Jim Green',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+  tags: ['loser'],
+}, {
+  key: '3',
+  name: 'Joe Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+  tags: ['cool', 'teacher'],
+}];
 
 class UserMgr extends MyView {
   constructor(props) {
@@ -10,7 +65,13 @@ class UserMgr extends MyView {
   }
 
   render() {
-    return (<h2>用户管理</h2>);
+    return (<div>
+      <div style={{marginBottom: 16}}>
+        <Button type="primary">添加</Button>
+        <Button type="info">删除</Button>
+      </div>
+      <Table columns={columns} dataSource={data}/>
+    </div>);
   }
 }
 
